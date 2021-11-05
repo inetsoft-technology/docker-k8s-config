aa6be6a97e1cc30a54da56974b51742b28efdf90
Jason Shobe <jason.shobe@inetsoft.com>
2021-11-05 14:55:12 -0400

    Prepare documentation for 2021 release.

--- config/scripts/01-remove-extras.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Removes the MySQL driver from the drivers directory so that it can
-# be used for the configuration/auditing database. The driver is included
-# in the lib directory.
-
-rm -f $STAGING_DIR/drivers/mysql-*.jar
